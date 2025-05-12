04086f34f294f7aeef5af58c014ac490fe21ea02
placebra <placebra404@gmail.com>
2025-05-13 00:02:11 +0300

    Добавлена БД, реализован Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,20 @@
 package jm.task.core.jdbc.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private static final String url = "jdbc:mysql://localhost:3306/pp1.3";
+    private static final String user = "Admin";
+    private static final String password = "Admin";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.out.println("Connection failed");
+            return null;
+        }
+    }
 }
